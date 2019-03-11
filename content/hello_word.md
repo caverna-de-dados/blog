fdfc022161a240c95e0121a4dea681e9ccb60e40
Diego Zurita <diego.zurita@usp.br>
2019-03-10 22:08:05 -0300

    Hello World

---
+++ content/hello_word.md
@@ -0,0 +1,10 @@
+Title: Hello World
+Date: 2019-10-03 19:23
+Modified: 2019-10-03 19:23
+Category: Python
+Tags: pelican, publishing
+Slug: hello-world
+Authors: Diego Zurita, Hugo Trigueiro
+Summary: Short version for index and feeds
+
+Hello wooooooorld!!
--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = 'pt'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,3 +33,93 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# STATIC_PATHS = ['assets']
+
+
+# EXTRA_PATH_METADATA = {
+#     'assets/robots.txt': {'path': 'robots.txt'},
+#     'assets/favicon.ico': {'path': 'favicon.ico'},
+#     'assets/CNAME': {'path': 'CNAME'}
+# }
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+### Plugins
+
+PLUGIN_PATHS = [
+  '/home/diego/Desktop/repos/blog/plugins'
+]
+
+PLUGINS = [
+  'sitemap',
+  'neighbors',
+  #'assets'
+]
+
+# Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+# Analytics
+#GOOGLE_ANALYTICS = "UA-3546274-12"
+
+THEME = 'attila'
+
+### Theme specific settings
+
+HEADER_COVER = 'https://casper.ghost.org/v1.0.0/images/welcome.jpg'
+
+COLOR_SCHEME_CSS = 'github.css'
+
+#CSS_OVERRIDE = ['assets/css/myblog.css']
+
+AUTHORS_BIO = {
+  "zutrinken": {
+    "name": "Zutrinken",
+    "cover": "https://casper.ghost.org/v1.0.0/images/team.jpg",
+    #"image": "assets/images/avatar.png",
+    "website": "http://blog.arulraj.net",
+    "linkedin": "unavailable",
+    "github": "arulrajnet",
+    "location": "Chennai",
+    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+  }
+}
+
+# Custom Header
+
+#HEADER_COVERS_BY_TAG = {'cupcake': 'assets/images/rainbow_cupcake_cover.png', 'general':'https://casper.ghost.org/v1.0.0/images/writing.jpg'}
+HEADER_COVERS_BY_TAG = {'general':'https://casper.ghost.org/v1.0.0/images/writing.jpg'}
+
+
