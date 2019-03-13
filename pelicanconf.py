d230fcd58dfe9af39c4b9c6676c2af3edd7bea88
Hugo Trigueiro <hugotrigueiro@users.noreply.github.com>
2019-03-12 22:28:48 -0300

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# STATIC_PATHS = ['assets']
+STATIC_PATHS = ['assets', 'static']
 
 
 # EXTRA_PATH_METADATA = {
