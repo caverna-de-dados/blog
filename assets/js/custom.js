3720ded1fae1e81583a7f3c82abd599e512d688a
Hugo Trigueiro <hvtrigueiro@gmail.com>
2019-05-07 13:40:15 -0300

    Update documentation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,2 @@
 $( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" );
 $("table").parents('div').css("overflow-x", "scroll");
-
-// var sharePromise = window.navigator.share(data);
