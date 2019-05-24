4b2a5688b3881d4a755677724f55bac5f5220bf0
Hugo Trigueiro <hvtrigueiro@gmail.com>
2019-05-24 00:56:07 -0300

    Update documentation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,2 +1,4 @@
-$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" );
-$("table").parents('div').css("overflow-x", "scroll");
+$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" ).css( "margin", "30px auto 10px" );
+// $( "span:not([class])" ).parent("pre").css( "margin", "0px auto 10px;" );
+// $("table").parents('div').css("overflow-x", "scroll");
+// margin: 30px auto 10px;
