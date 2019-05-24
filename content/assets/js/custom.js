218019e194339d0aba10da438ecfe2a252de791b
Hugo Trigueiro <hvtrigueiro@gmail.com>
2019-05-24 20:41:05 -0300

    atualização do design dos inputs e outputs

--- content/assets/js/custom.js
+++ content/assets/js/custom.js
@@ -1,4 +1,4 @@
-$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" ).css( "margin", "10px auto" ).css("background-color", "rgb(255, 251, 251)");
+$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" ).css( "margin", "10px auto" ).css("background-color", "rgb(255, 251, 251)").css('background-color', '#000').css('color', '#fff');
 $( ".dataframe" ).parent( "div" ).css( "margin-bottom", "30px" );
 // $( "span:not([class])" ).parent("pre").css( "margin", "0px auto 10px;" );
 // $("table").parents('div').css("overflow-x", "scroll");
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,8 +6,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Caverneiros'
 SITENAME = 'Caverna de Dados'
-SITEURL = 'https://cavernadedados.com'
-# SITEURL = ''
+# SITEURL = 'https://cavernadedados.com'
+SITEURL = ''
 SITE_LOGO = '/assets/img/logo.png'
 
 PATH = 'content/'
