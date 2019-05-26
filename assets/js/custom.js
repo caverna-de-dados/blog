759348fd355b528479bcac93d16af37f00bd3fd9
Hugo Trigueiro <hvtrigueiro@gmail.com>
2019-05-26 00:46:00 -0300

    Update documentation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,2 +1,2 @@
-$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" ).css( "margin", "10px auto 3px" ).css("background-color", "rgb(255, 251, 251)").css('background-color', '#191919').css('color', '#fff').css('padding', '8px 10px');
+$( ".hll, .n, .c, .err, .s1, .ss, .bp, .vc, .vg, .vi, .il" ).parent( "pre" ).css( "border", "1px solid #DEDEDE" ).css( "margin", "10px auto 3px" ).css("background-color", "rgb(255, 251, 251)").css('background-color', '#191919').css('color', '#fff').css('padding', '8px 10px').css('white-space', 'pre');
 $( ".dataframe" ).parent( "div" ).css( "margin-bottom", "35px" );
